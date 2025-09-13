b70bcaf066aa084710ab7d784a1fbe6626da2527
brianjtarte <brianjtarte@gmail.com>
2025-09-13 07:37:18 -0700

    added type declaration to auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,17 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
 import { prisma } from './db';
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
