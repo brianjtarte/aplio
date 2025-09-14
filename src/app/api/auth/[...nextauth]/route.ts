269ac2290015fac7283de79430f810f087dc880e
brianjtarte <brianjtarte@gmail.com>
2025-09-14 07:26:23 -0700

    updated nextauth route.ts to dynamic import

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/auth';
 
-const handler = NextAuth(authOptions);
+export const GET = async (req: any, res: any) => {
+  const { authOptions } = await import('@/lib/auth');
+  return NextAuth(authOptions).handlers.GET(req, res);
+};
 
-export { handler as GET, handler as POST };
+export const POST = async (req: any, res: any) => {
+  const { authOptions } = await import('@/lib/auth');
+  return NextAuth(authOptions).handlers.POST(req, res);
+};
